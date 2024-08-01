16523270ee75e56b8408abfeea274b7c5e2e2273
eduardooliveiraa <eeduardo.og11@gmail.com>
2024-07-31 23:25:24 -0300

    Adding a script to althenticate login

---
+++ script.js
@@ -0,0 +1,29 @@
+function logar() {
+  var login = document.getElementById('login').value;
+  var senha = document.getElementById('senha').value;
+
+  if (login == 'admin' && senha == 'admnin') {
+    alert('Sucesso');
+  }
+  else{
+    alert('Usuario ou senha incorretos');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
