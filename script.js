b3dcb512c087222595154da7d91d1d8dfa3b6d4b
eduardooliveiraa <eeduardo.og11@gmail.com>
2024-08-01 22:04:47 -0300

    js update, including redirection to a new page for accepted user

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function logar() {
   var login = document.getElementById('login').value;
   var senha = document.getElementById('senha').value;
 
-  if (login == 'admin' && senha == 'admnin') {
+  if (login == 'admin' && senha == 'admin') {
     alert('Sucesso');
+    window.location.href='pagina-usuario.html'
   }
-  else {
+  else{
     alert('Usuario ou senha incorretos');
   }
 }
